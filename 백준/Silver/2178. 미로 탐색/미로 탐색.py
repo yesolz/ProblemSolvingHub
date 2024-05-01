8bf1328d9f99eb90d78c643629e05a5dc327aead
yesolz <112631986+yesolz@users.noreply.github.com>
2024-05-01 09:32:40 +0900

    [Silver I] Title: 미로 탐색, Time: 84 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+n, m = map(int, input().split())
+maze = [list(map(int, input())) for _ in range(n)]
+
+# 네 가지 방향
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            # 공간 벗어난 경우 무시
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            # 벽인 경우 무시
+            if maze[nx][ny] == 0:
+                continue
+            # 방문
+            if maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1
+                queue.append((nx, ny))
+
+    return maze[n-1][m-1]
+
+print(bfs(0,0))
