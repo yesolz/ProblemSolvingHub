a42c1d1c96bad082baee67a6badc0f529aad1546
yesolz <112631986+yesolz@users.noreply.github.com>
2024-05-16 01:16:03 +0900

    [Silver V] Title: 집합, Time: 3796 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input().strip())
+s = set()
+
+for _ in range(M):
+    command = input().strip().split()
+    if len(command) == 2:
+        op, x = command[0], int(command[1])
+        if op == "add":
+            s.add(x)
+        elif op == "remove":
+            s.discard(x)
+        elif op == "check":
+            print(1 if x in s else 0)
+        elif op == "toggle":
+            if x in s:
+                s.remove(x)
+            else:
+                s.add(x)
+    else:
+        if command[0] == "all":
+            s = set(range(1, 21))
+        elif command[0] == "empty":
+            s.clear()
