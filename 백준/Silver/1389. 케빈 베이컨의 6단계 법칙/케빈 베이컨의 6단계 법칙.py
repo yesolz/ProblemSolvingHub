e5de6ccd4fa987a63bc81449639c76f290abfe75
yesolz <112631986+yesolz@users.noreply.github.com>
2024-05-20 23:42:34 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 64 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,36 +1,46 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
+# 사용자 수 N, 친구 관계 수 M 입력
 N, M = map(int, input().split())
 
-# 거리 배열 초기화
-INF = sys.maxsize
-dist = [[INF] * (N+1) for _ in range(N+1)]
-
-# 자신과의 거리는 0으로
-for i in range(1, N+1):
-    dist[i][i] = 0
+# 그래프 초기화
+graph = [[] for _ in range(N + 1)]
 
 # 친구 관계 입력
 for _ in range(M):
     A, B = map(int, input().split())
-    dist[A][B] = 1
-    dist[B][A] = 1
+    graph[A].append(B)
+    graph[B].append(A)
 
-# 플로이드-워셜
-for k in range(1, N+1):
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            if dist[i][j] > dist[i][k] + dist[k][j]:
-                dist[i][j] = dist[i][k] + dist[k][j]
+# BFS 함수 정의
+def bfs(start):
+    distances = [-1] * (N + 1)
+    queue = deque([start])
+    distances[start] = 0
+    
+    while queue:
+        current = queue.popleft()
+        
+        for neighbor in graph[current]:
+            if distances[neighbor] == -1:
+                distances[neighbor] = distances[current] + 1
+                queue.append(neighbor)
+    
+    return distances
 
-min_bacon_num = INF
+# 모든 사용자에 대해 케빈 베이컨 수 계산
+min_bacon_number = sys.maxsize
 result_person = -1
 
-for i in range(1, N+1):
-    bacon_num = sum(dist[i][1:])
-    if bacon_num < min_bacon_num:
-        min_bacon_num = bacon_num
+for i in range(1, N + 1):
+    distances = bfs(i)
+    bacon_number = sum(distances[1:])  # 1번 사용자부터 N번 사용자까지의 거리 합
+    
+    if bacon_number < min_bacon_number:
+        min_bacon_number = bacon_number
         result_person = i
 
+# 결과 출력
 print(result_person)
