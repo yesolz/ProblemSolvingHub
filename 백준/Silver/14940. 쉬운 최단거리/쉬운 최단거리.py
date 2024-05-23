d2e6e3503fe1be1749cae10d510d5e556f2f4835
yesolz <112631986+yesolz@users.noreply.github.com>
2024-05-23 18:33:34 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 248 ms, Memory: 121464 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -2,34 +2,31 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-def bfs(grid, n, m):
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+def bfs(graph, n, m):
 
     distance = [[-1] * m for _ in range(n)]
     queue = deque()
 
     for i in range(n):
         for j in range(m):
-            if grid[i][j] == 2:
+            if graph[i][j] == 2:
                 queue.append((i, j))
                 distance[i][j] = 0
-            elif grid[i][j] == 0:
+            elif graph[i][j] == 0:
                 distance[i][j] = 0
-
     while queue:
         x, y = queue.popleft()
 
-        for dx, dy in directions:
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             nx, ny = x + dx, y + dy
             if 0 <= nx < n and 0 <= ny < m:
-                if grid[nx][ny] == 1 and distance[nx][ny] == -1:
+                if graph[nx][ny] == 1 and distance[nx][ny] == -1:
                     distance[nx][ny] = distance[x][y] + 1
                     queue.append((nx, ny))
-
     return distance
 
-distances = bfs(grid, n, m)
+distances = bfs(graph, n, m)
 for row in distances:
-    print(' '.join(map(str, row)))
+    print(*row)
